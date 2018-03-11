a8c9e8f6caac6c929e1b89a5374e94a956b4d9ea
Olaf Fiedler <mail@olaf-fiedler.de>
2018-03-11 21:35:01 +0100

    Added basic use case tests.

--- fndiff/tests/test_fndiff.py
+++ fndiff/tests/test_fndiff.py
@@ -5,14 +5,40 @@
 """
 from unittest import TestCase
 
-# from fndiff import fndiff, FilenamesDiffer, FilenamesDiffError
 import fndiff
+
+import os
+TESTDATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
+
 
 class TestFnDiff (TestCase):
 
+    def setUp(self):
+        self.jpg_dir = os.path.join(TESTDATA_PATH, 'JPG')
+        self.raf_dir = os.path.join(TESTDATA_PATH, 'RAF')
+        self.fuji_pattern = 'DSCF([0-9]+)[.].*$'
+
     def test_trivial(self):
+        """Just ensure tests are found and running in general."""
         self.assertTrue(True)
 
-    def test_diff_creation(self):
+    def test_diff_creation_fail(self):
+        """Assert there's no FilenamesDiffer creation without arg's."""
         with self.assertRaises(fndiff.FilenamesDiffError):
             fndiff.FilenamesDiffer()
+
+    def test_diff_usual(self):
+        """Test usual case of missing JPGs in comparison to RAF."""
+        raf_expected_result_set = { os.path.join(self.raf_dir, x)
+            for x in [ 'DSCF01236.RAF', 'DSCF01234.RAF', 'DSCF01231.RAF',
+                        'DSCF01233.RAF', 'DSCF01232.RAF']}
+        result = fndiff.filenames_diff(self.raf_dir, self.jpg_dir,
+            self.fuji_pattern, True)
+        self.assertSetEqual(set(map(lambda x: str(x), result)),
+            raf_expected_result_set)
+        
+    def test_diff_same_dir_error(self):
+        """Assert failure on source directory == target directory."""
+        with self.assertRaises(fndiff.FilenamesDiffError):
+            fndiff.filenames_diff(self.jpg_dir, self.jpg_dir,
+                self.fuji_pattern, True)
